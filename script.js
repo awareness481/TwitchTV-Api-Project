fe2f699de0b656edcaf8e4f5ab009d09b9ea12ca
Jesse Jafa <jesse.jafa@gmail.com>
2017-08-02 19:43:27 +0300

    Added .eslintrc file, using Airbnb style

--- script.js
+++ script.js
@@ -1,23 +1,36 @@
+/**
+ * [fullUrl description]
+ * @param  {Strings} arr [Array of users names]
+ * @return {String}      [String including url]
+ */
 function fullUrl(arr) {
   const url = 'https://wind-bow.glitch.me/twitch-api/users/';
-  return arr.map(arr => (url + arr));
+  return arr.map(user => (url + user));
 }
 
+
+/**
+ * Return fetched json to var inside generator
+ * @param  {Array} arr [Usernames including URK]
+ * @return {Object}     [Implicit return object arrays]
+ */
 function allIds(arr) {
-  arr = fullUrl(arr);
   Promise.all(arr.map(link => fetch(link)
-    .then(link => link.json()))) //end .map
+    .then(data => data.json()))) // end .map
     .then(links => iterator.next(links));
 }
 
-function *apiCalls() {
-  let userIds = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp",
-  "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+/**
+ * Generator
+ * @return {Generator} [description]
+ */
+function* apiCalls() {
+  let userIds = ['ESL_SC2', 'OgamingSC2', 'cretetion', 'freecodecamp',
+    'storbeck', 'habathcx', 'RobotCaleb', 'noobs2ninjas'];
+  userIds = fullUrl(userIds);
 
   const users = yield allIds(userIds);
   console.log(users);
-
-
 }
 
 const iterator = apiCalls();
